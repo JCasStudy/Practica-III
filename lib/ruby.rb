29d1d21e7a25c3cec92956ad77627768ba339af9
Jose Carlos Rios <jose.flores220@est.unanleon.edu.ni>
2024-09-13 15:10:38 -0600

    Practica 3

---
+++ lib/ruby.rb
@@ -0,0 +1,14 @@
+def intro array
+  #escribir el código
+  array.sum
+end
+def nombre nom
+  #escribir el código
+  return "vacío" if nom.empty?
+  "hello #{nom}"
+end
+def datos? array
+  #escribir el código
+  !array.empty?
+end
+
